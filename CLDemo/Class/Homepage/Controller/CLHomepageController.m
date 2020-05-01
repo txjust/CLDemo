c66b07f3f2752d900de3c59bea0de7b88c7fea50
JmoVxia <269968846@qq.com>
2020-05-01 14:36:33 +0800

    fix

--- CLDemo/Class/Homepage/Controller/CLHomepageController.m
+++ CLDemo/Class/Homepage/Controller/CLHomepageController.m
@@ -103,6 +103,7 @@
         [_arrayDS addObject:NSLocalizedString(@"水平布局", nil)];
         [_arrayDS addObject:NSLocalizedString(@"标签动态排布", nil)];
         [_arrayDS addObject:NSLocalizedString(@"自定义弹窗", nil)];
+        [_arrayDS addObject:NSLocalizedString(@"游标卡尺", nil)];
         [_arrayDS addObject:NSLocalizedString(@"翻转动画", nil)];
         [_arrayDS addObject:NSLocalizedString(@"聊天框架", nil)];
         [_arrayDS addObject:NSLocalizedString(@"广播轮播", nil)];
@@ -143,6 +144,7 @@
         [_controllerArray addObject:[CLLayoutController class]];
         [_controllerArray addObject:[CLTagsController class]];
         [_controllerArray addObject:[CLPopupController class]];
+        [_controllerArray addObject:[CLVernierCaliperController class]];
         [_controllerArray addObject:[CLFlipController class]];
         [_controllerArray addObject:[CLChatController class]];
         [_controllerArray addObject:[CLBroadcastViewController class]];
---
+++ CLDemo/Class/Homepage/Controller/CLVernierCaliperController/CLVernierCaliperView/CLVernierCaliperView.swift
@@ -0,0 +1,21 @@
+//
+//  CLVernierCaliperView.swift
+//  CLDemo
+//
+//  Created by JmoVxia on 2020/5/1.
+//  Copyright © 2020 JmoVxia. All rights reserved.
+//
+
+import UIKit
+
+class CLVernierCaliperView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
