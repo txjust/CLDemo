867f142c561a5d56c7cd0628ed466f4dfb1dc544
JmoVxia <269968846@qq.com>
2020-06-29 16:21:39 +0800

    fix

--- CLDemo/Class/Homepage/Controller/CLRecordeEncodeController/CLRecordeEncodeController.m
+++ CLDemo/Class/Homepage/Controller/CLRecordeEncodeController/CLRecordeEncodeController.m
@@ -28,7 +28,6 @@ static void set_bits(uint8_t *bytes, int32_t bitOffset, int32_t numBits, int32_t
 @property (nonatomic, strong) UIButton *playButton;
 @property (nonatomic, strong) CLRecorder *recorder;
 @property (nonatomic, strong) CLChatVoiceWave *waveView;
-@property (nonatomic, strong) CLChatVoiceWave *waveView1;
 @property (nonatomic, strong) CLVoicePlayer *player;
 
 @end
@@ -41,7 +40,6 @@ static void set_bits(uint8_t *bytes, int32_t bitOffset, int32_t numBits, int32_t
     [self.view addSubview:self.startButton];
     [self.view addSubview:self.playButton];
     [self.view addSubview:self.waveView];
-    [self.view addSubview:self.waveView1];
     [self.startButton mas_makeConstraints:^(MASConstraintMaker *make) {
         make.left.mas_equalTo(90);
         make.top.mas_equalTo(200);
@@ -54,14 +52,7 @@ static void set_bits(uint8_t *bytes, int32_t bitOffset, int32_t numBits, int32_t
         make.center.mas_equalTo(self.view);
         make.size.mas_equalTo(CGSizeMake(200, 50));
     }];
-    [self.waveView1 mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.centerX.mas_equalTo(self.view);
-        make.top.mas_equalTo(self.waveView.mas_bottom).mas_offset(40);
-        make.size.mas_equalTo(CGSizeMake(200, 50));
-    }];
-
     self.waveView.peakHeight = 50;
-    self.waveView1.peakHeight = 50;
 }
 
 - (void)startAction {
@@ -76,9 +67,6 @@ static void set_bits(uint8_t *bytes, int32_t bitOffset, int32_t numBits, int32_t
         if (self.recorder.mp3Path.length > 0) {
             NSData *waveSamples = [self audioWaveform];
             self.waveView.waveData = waveSamples;
-            
-            NSData *waveSamples1 = [self audioWaveform:[NSURL fileURLWithPath:self.recorder.mp3Path]];
-            self.waveView1.waveData = waveSamples1;
         }
     }
     self.startButton.selected = !self.startButton.selected;
@@ -93,147 +81,6 @@ static void set_bits(uint8_t *bytes, int32_t bitOffset, int32_t numBits, int32_t
         self.playButton.selected = !self.playButton.selected;
     }
 }
-- (NSData *)audioWaveform:(NSURL *)url {
-//    NSTimeInterval start = [[NSDate date] timeIntervalSince1970];
-    NSDictionary *outputSettings = [NSDictionary dictionaryWithObjectsAndKeys:
-                                    [NSNumber numberWithInt:kAudioFormatLinearPCM], AVFormatIDKey,
-                                    [NSNumber numberWithFloat:44100.0], AVSampleRateKey,
-                                    [NSNumber numberWithInt:16], AVLinearPCMBitDepthKey,
-                                    [NSNumber numberWithBool:NO], AVLinearPCMIsNonInterleaved,
-                                    [NSNumber numberWithBool:NO], AVLinearPCMIsFloatKey,
-                                    [NSNumber numberWithBool:NO], AVLinearPCMIsBigEndianKey,
-                                    nil];
-    AVURLAsset *asset = [AVURLAsset URLAssetWithURL:url options:nil];
-    CMTime audioDuration = asset.duration;
-    float audioDurationSeconds = CMTimeGetSeconds(audioDuration);
-    CLLog(@"获取到时长为 %f", audioDurationSeconds);
-
-    if (asset == nil) {
-        CLLog(@"asset is not defined!");
-        return nil;
-    }
-    
-    NSError *assetError = nil;
-    AVAssetReader *iPodAssetReader = [AVAssetReader assetReaderWithAsset:asset error:&assetError];
-    if (assetError) {
-        CLLog (@"error: %@", assetError);
-        return nil;
-    }
-    
-    AVAssetReaderOutput *readerOutput = [AVAssetReaderAudioMixOutput assetReaderAudioMixOutputWithAudioTracks:asset.tracks audioSettings:outputSettings];
-    
-    if (! [iPodAssetReader canAddOutput: readerOutput]) {
-        CLLog (@"can't add reader output... die!");
-        return nil;
-    }
-    
-    // add output reader to reader
-    [iPodAssetReader addOutput: readerOutput];
-    
-    if (! [iPodAssetReader startReading]) {
-        CLLog(@"Unable to start reading!");
-        return nil;
-    }
-//    NSTimeInterval end = [[NSDate date] timeIntervalSince1970];
-//    NSLog(@"%f",end - start);
-
-    NSMutableData *_waveformSamples = [[NSMutableData alloc] init];
-    int16_t _waveformPeak = 0;
-    int _waveformPeakCount = 0;
-    
-    while (iPodAssetReader.status == AVAssetReaderStatusReading) {
-        // Check if the available buffer space is enough to hold at least one cycle of the sample data
-        CMSampleBufferRef nextBuffer = [readerOutput copyNextSampleBuffer];
-        
-        if (nextBuffer) {
-            AudioBufferList abl;
-            CMBlockBufferRef blockBuffer = NULL;
-            CMSampleBufferGetAudioBufferListWithRetainedBlockBuffer(nextBuffer, NULL, &abl, sizeof(abl), NULL, NULL, kCMSampleBufferFlag_AudioBufferList_Assure16ByteAlignment, &blockBuffer);
-            UInt64 size = CMSampleBufferGetTotalSampleSize(nextBuffer);
-            if (size != 0) {
-                int16_t *samples = (int16_t *)(abl.mBuffers[0].mData);
-                int count = (int)size / 2;
-                
-                for (int i = 0; i < count; i++) {
-                    int16_t sample = samples[i];
-                    if (sample < 0) {
-                        sample = -sample;
-                    }
-                    
-                    if (_waveformPeak < sample) {
-                        _waveformPeak = sample;
-                    }
-                    _waveformPeakCount++;
-                    
-                    if (_waveformPeakCount >= 100) {
-                        [_waveformSamples appendBytes:&_waveformPeak length:2];
-                        _waveformPeak = 0;
-                        _waveformPeakCount = 0;
-                    }
-                }
-            }
-            
-            CFRelease(nextBuffer);
-            if (blockBuffer) {
-                CFRelease(blockBuffer);
-            }
-        }
-        else {
-            break;
-        }
-    }
-    
-    int16_t scaledSamples[100];
-    memset(scaledSamples, 0, 100 * 2);
-    int16_t *samples = _waveformSamples.mutableBytes;
-    int count = (int)_waveformSamples.length / 2;
-    for (int i = 0; i < count; i++) {
-        int16_t sample = samples[i];
-        int index = i * 100 / count;
-        if (scaledSamples[index] < sample) {
-            scaledSamples[index] = sample;
-        }
-    }
-    
-    int16_t peak = 0;
-    int64_t sumSamples = 0;
-    for (int i = 0; i < 100; i++) {
-        int16_t sample = scaledSamples[i];
-        if (peak < sample) {
-            peak = sample;
-        }
-        sumSamples += sample;
-    }
-    uint16_t calculatedPeak = 0;
-    calculatedPeak = (uint16_t)(sumSamples * 1.8f / 100);
-    
-    if (calculatedPeak < 2500) {
-        calculatedPeak = 2500;
-    }
-    
-    for (int i = 0; i < 100; i++) {
-        uint16_t sample = (uint16_t)((int64_t)samples[i]);
-        if (sample > calculatedPeak) {
-            scaledSamples[i] = calculatedPeak;
-        }
-    }
-    
-    int numSamples = 100;
-    int number = 5;
-    int bitstreamLength = (numSamples * number) / 8 + (((numSamples * number) % 8) == 0 ? 0 : 1);
-    NSMutableData *result = [[NSMutableData alloc] initWithLength:bitstreamLength];
-    {
-        int32_t maxSample = peak;
-        uint16_t const *samples = (uint16_t *)scaledSamples;
-        uint8_t *bytes = result.mutableBytes;
-        
-        for (int i = 0; i < numSamples; i++) {
-            int32_t value = MIN(31, ABS((int32_t)samples[i]) * 31 / maxSample);
-            set_bits(bytes, i * number, number, value & 31);
-        }
-    }
-    return result;
-}
 - (NSData *)audioWaveform {
     int sampleCount = 100;
     int16_t scaledSamples[sampleCount];
@@ -247,45 +94,44 @@ static void set_bits(uint8_t *bytes, int32_t bitOffset, int32_t numBits, int32_t
             scaledSamples[index] = sample;
         }
     }
-    return [NSData dataWithBytes:scaledSamples length:sampleCount];
-//    int16_t peak = 0;
-//    int64_t sumSamples = 0;
-//    for (int i = 0; i < sampleCount; i++) {
-//        int16_t sample = scaledSamples[i];
-//        if (peak < sample) {
-//            peak = sample;
-//        }
-//        sumSamples += sample;
-//    }
-//    uint16_t calculatedPeak = 0;
-//    calculatedPeak = (uint16_t)(sumSamples * 1.8f / sampleCount);
-//
-//    if (calculatedPeak < 2500) {
-//        calculatedPeak = 2500;
-//    }
-//
-//    for (int i = 0; i < sampleCount; i++) {
-//        uint16_t sample = (uint16_t)((int64_t)samples[i]);
-//        if (sample > calculatedPeak) {
-//            scaledSamples[i] = calculatedPeak;
-//        }
-//    }
-//
-//    int numSamples = sampleCount;
-//    int number = 5;
-//    int bitstreamLength = (numSamples * number) / 8 + (((numSamples * number) % 8) == 0 ? 0 : 1);
-//    NSMutableData *result = [[NSMutableData alloc] initWithLength:bitstreamLength];
-//    {
-//        int32_t maxSample = peak;
-//        uint16_t const *samples = (uint16_t *)scaledSamples;
-//        uint8_t *bytes = result.mutableBytes;
-//
-//        for (int i = 0; i < numSamples; i++) {
-//            int32_t value = MIN(31, ABS((int32_t)samples[i]) * 31 / maxSample);
-//            set_bits(bytes, i * number, number, value & 31);
-//        }
-//    }
-//    return result;
+    int16_t peak = 0;
+    int64_t sumSamples = 0;
+    for (int i = 0; i < sampleCount; i++) {
+        int16_t sample = scaledSamples[i];
+        if (peak < sample) {
+            peak = sample;
+        }
+        sumSamples += sample;
+    }
+    uint16_t calculatedPeak = 0;
+    calculatedPeak = (uint16_t)(sumSamples * 1.8f / sampleCount);
+
+    if (calculatedPeak < 2500) {
+        calculatedPeak = 2500;
+    }
+
+    for (int i = 0; i < sampleCount; i++) {
+        uint16_t sample = (uint16_t)((int64_t)samples[i]);
+        if (sample > calculatedPeak) {
+            scaledSamples[i] = calculatedPeak;
+        }
+    }
+
+    int numSamples = sampleCount;
+    int number = 5;
+    int bitstreamLength = (numSamples * number) / 8 + (((numSamples * number) % 8) == 0 ? 0 : 1);
+    NSMutableData *result = [[NSMutableData alloc] initWithLength:bitstreamLength];
+    {
+        int32_t maxSample = peak;
+        uint16_t const *samples = (uint16_t *)scaledSamples;
+        uint8_t *bytes = result.mutableBytes;
+
+        for (int i = 0; i < numSamples; i++) {
+            int32_t value = MIN(31, ABS((int32_t)samples[i]) * 31 / maxSample);
+            set_bits(bytes, i * number, number, value & 31);
+        }
+    }
+    return result;
 }
 
 
@@ -317,12 +163,6 @@ static void set_bits(uint8_t *bytes, int32_t bitOffset, int32_t numBits, int32_t
         _waveView = [[CLChatVoiceWave alloc] init];
     }
     return _waveView;
-}
-- (CLChatVoiceWave *)waveView1 {
-    if (!_waveView1) {
-        _waveView1 = [[CLChatVoiceWave alloc] init];
-    }
-    return _waveView1;
 }
 - (CLVoicePlayer *)player {
     if (!_player) {
