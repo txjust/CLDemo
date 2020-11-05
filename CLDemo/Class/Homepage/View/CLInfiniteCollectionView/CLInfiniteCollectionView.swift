f857c11d85f24662f43a3bfd82cae923f7f66b82
Chen JmoVxia <>
2020-11-05 10:59:56 +0800

    fix

--- CLDemo/Class/Homepage/View/CLInfiniteCollectionView/CLInfiniteCollectionView.swift
+++ CLDemo/Class/Homepage/View/CLInfiniteCollectionView/CLInfiniteCollectionView.swift
@@ -30,6 +30,7 @@ class CLInfiniteCollectionView: UICollectionView {
         backgroundColor = .white
         showsVerticalScrollIndicator = false
         showsHorizontalScrollIndicator = false
+        decelerationRate = .fast
     }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
